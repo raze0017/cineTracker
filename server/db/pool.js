fd740c72c775ff277ffd791fdc398b6d9ef8fa91
raze0017 <appurazeem2000@gmail.com>
2025-02-23 22:39:36 +0530

    host database in render

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
-module.exports = new Pool({
+/*module.exports = new Pool({
   host: "localhost", // or wherever the db is hosted
   user: "raze",
   database: "cinema",
   password: "raze",
   port: 5432, // The default port
+});*/
+
+module.exports = new Pool({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://cinema_yqio_user:An4vsFX7qVLrJX0UP0InfEpWEoQTm0sC@dpg-cutko7ij1k6c738dd5fg-a.oregon-postgres.render.com/cinema_yqio",
+  ssl: { rejectUnauthorized: false }, // Required for Render
 });
