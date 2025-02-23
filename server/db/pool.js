13e3999f79733e54757ad8c150c67eb3ffab4943
raze0017 <appurazeem2000@gmail.com>
2025-02-23 23:31:08 +0530

    fix

--- server/db/pool.js
+++ server/db/pool.js
@@ -10,6 +10,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   connectionString:
-    process.env.DATABASE_URL || "postgresql://raze:raze@localhost:5432/cinema",
+    process.env.DATABASE_URL ||
+    "postgresql://cinema_yqio_user:An4vsFX7qVLrJX0UP0InfEpWEoQTm0sC@dpg-cutko7ij1k6c738dd5fg-a.oregon-postgres.render.com/cinema_yqio",
   ssl: { rejectUnauthorized: false }, // Required for Render
 });
